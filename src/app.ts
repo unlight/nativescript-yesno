a008cbc36d14e6f60a321b91cd06c36b774e2893
Roman <s91630277@gmail.com>
2016-01-18 16:16:49 +0300

    Working version

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 /// <reference path="references.d.ts" />
 import application = require("application");
-application.mainModule = "./main/index";
+// application.mainModule = "./main/index";
+application.mainModule = "main-page";
 application.cssFile = "./app.css";
 application.start();
