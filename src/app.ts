b12fa90e06370e216a2109989b8b3db7077bb463
roman.vasilev <s91630277@gmail.com>
2016-01-23 01:02:25 +0300

    Moved to views

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 /// <reference path="references.d.ts" />
 import application = require("application");
 // application.mainModule = "./main/index";
-application.mainModule = "main-page";
+application.mainModule = "./views/main";
 application.cssFile = "./app.css";
 application.start();
